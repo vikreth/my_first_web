3cbdf9a7e0e7fc8b2184fd933fe6f254bb6b2170
Vikreth <unvikreth@gmail.com>
2023-04-12 14:10:22 +0700

    initialize

---
+++ my_website/js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    const date = new Date();
+    const hour = date.getHours();
+    let message = '';
+    if(hour<=12){
+        message = 'Good Morning'
+    }
+    if(hour>12){
+        message = 'Good Afternoon'
+    }
+    if(hour>=17){
+        message = 'Good Evening'
+    }
+    $('#headingText').empty().append(message + " From Vikreth");
+
+})
